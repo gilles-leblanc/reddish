671ef3f5776e3bf27776c7cd7df856713670c5b9
Gilles Leblanc <gilles.leblanc@gmail.com>
2014-10-25 11:34:40 -0400

    Basic linked list structure
    Includes a println! statement to check first value.

--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-extern crate reddish_lib;
-
-use reddish_lib::linkedlist;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 extern crate reddish_lib;
 
-use reddish_lib::linkedlist;
+use reddish_lib::linkedlist::{Node, Nil};
 
 fn main() {
-    //let list = Node(1, box Node(2, box Node(3, box Nil)));
-    println!("Hello")
+    let list = Node(1, box Node(2, box Node(3, box Nil)));
+
+    match list {
+        Node(first_value, _) => println!("First value {}", first_value),
+        Nil => println!("Nil value")
+    }
 }
