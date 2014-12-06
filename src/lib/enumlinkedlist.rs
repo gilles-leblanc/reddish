4ac8db317b39cf76f87832ead398220093b850d6
Gilles Leblanc <gilles.leblanc@gmail.com>
2014-12-06 10:50:20 -0500

    wip

--- src/lib/enumlinkedlist.rs
+++ src/lib/enumlinkedlist.rs
@@ -3,4 +3,37 @@ pub enum EnumLinkedList {
     Nil
 }
 
+impl EnumLinkedList {
+    fn append(&mut self, element: u32) {
+        match *self {
+            Node(_, ref mut next) => next.append(element),
+            Nil => *self = Node(element, box Nil),
+        }
+    }
 
+    fn next(&self) -> &EnumLinkedList {
+        match *self {
+            Node(_, next) => &*next,
+            Nil => &Nil,
+        }
+    }
+}
+
+
+#[test]
+fn next_returns_next_element() {
+    let list = Node(1, box Node(2, box Nil));
+
+    match list.next() {
+        Node(2, _) => assert!(true),
+        _ => assert!(false),
+    }
+}
+
+#[test]
+fn append_node_to_list() {
+    let list = Node(1, box Node(2, box Nil));
+    list.append(4);
+
+
+}
